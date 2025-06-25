efac34b8942bab4de6b7c0de6d1c6b537ad3afad
the-white-fang <admin@filthyweebs.in>
2025-06-25 14:32:45 +0530

    added anime-cards in anime search

--- services/anime.ts
+++ services/anime.ts
@@ -11,6 +11,21 @@ export type FetchFilterResponse = {
 	licensors: Filter[];
 	studios: Filter[];
 	themes: Filter[];
+	streaming_services: Filter[];
+};
+
+export type AnimeShort = {
+	id: number;
+	image_url: string;
+	title: string;
+	genres: {
+		id: number;
+		name: string;
+	}[];
+	type: string | null;
+	episodes: number | null;
+	status: string | null;
+	synopsis: string;
 };
 
 export async function fetch_filters() {
@@ -19,9 +34,40 @@ export async function fetch_filters() {
 	const body = await response.json().catch(() => null);
 
 	if (!response.ok) {
-		const fallback = response.status >= 500 ? 'Server error while loading filters' : 'Unable to load filters. Please try again later.';
+		const fallback = response.status >= 500 ? 'Server error while loading filters. Please try again later.' : 'Unable to load filters.';
 		throw new Error(body?.message || fallback);
 	}
 
 	return body as FetchFilterResponse;
+}
+
+export async function search_anime(search_query: string, filter: Record<keyof FetchFilterResponse, number[]>, page: number = 1, page_size: number = 20) {
+	const params = new URLSearchParams();
+
+	params.set('search', search_query);
+	params.set('page', page.toString());
+	params.set('page_size', page_size.toString());
+
+	for (const key in filter) {
+		const value = filter[key as keyof FetchFilterResponse];
+
+		if (!value.length) {
+			continue;
+		}
+
+		for (const id of value) {
+			params.append(key, id.toString());
+		}
+	}
+
+	const response = await fetch(`${api}/v1/anime/search?${params.toString()}`);
+
+	const body = await response.json().catch(() => null);
+
+	if (!response.ok) {
+		const fallback = response.status >= 500 ? 'Server error while loading filters. Please try again later.' : 'Unable to load filters.';
+		throw new Error(body?.message || fallback);
+	}
+
+	return body as { animes: AnimeShort[] };
 }
